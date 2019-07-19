096afbce0b117ce18737722ad96e613d79b3618e
Bruno Goncalves <bgoncalv@redhat.com>
2019-07-19 10:33:28 +0200

    run tests using standard-test-basic role
    attempt to run the test also as rootless

--- tests/test_podman.sh
+++ tests/test_podman.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # Simple podman tests
 #
 
-rm -f /tmp/test.log /tmp/test.debug.log
-
 # Log program versions
-rpm -q podman podman-tests >/tmp/test.debug.log
+rpm -q podman podman-tests
 
-bats /usr/share/podman/test/system &> /tmp/test.log
-
-echo "bats completed with status $?" >>/tmp/test.debug.log
+bats /usr/share/podman/test/system
