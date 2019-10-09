a68b9cccc38431b1b1c04941cf54c6f11982f3ae
Ed Santiago <santiago@redhat.com>
2019-10-09 06:35:30 -0600

    gating tests: also log environment
    Signed-off-by: Ed Santiago <santiago@redhat.com>

--- tests/test_podman.sh
+++ tests/test_podman.sh
@@ -10,4 +10,8 @@ echo "Important package versions:"
     rpm -qa | egrep 'podman|conmon|crun|runc|iptable|slirp|systemd' | sort
 ) | sed -e 's/^/  /'
 
+# Log environment; or at least the useful bits
+echo "Environment:"
+env | grep -v LS_COLORS= | sort | sed -e 's/^/  /'
+
 bats /usr/share/podman/test/system
